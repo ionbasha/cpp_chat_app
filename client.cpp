8ccee3214653ab2c5ac6284b8a76bbca3a0010b2
Ion Basha <ionbasha15@gmail.com>
2025-07-17 15:44:45 -0700

    Added basic server and client functionalty + commented notes

--- client.cpp
+++ client.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h> 
+#include <arpa/inet.h>
 
 
 int main() {
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int portNumber = 3000;
     if (client_fd < 0) {
         std::cerr << "Could not create socket. Please try again.";
         exit(0);
     }
     sockaddr_in server_address;
     server_address.sin_family = AF_INET; // Will connect to server via IPv4 domain
-    server_address.sin_port = htons(8080);
+    server_address.sin_port = htons(portNumber);
+
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr); // Set server IP
+
+    int server_connection = connect(client_fd, (sockaddr*)&server_address, sizeof(server_address));
+
+    if (server_connection < 0) {
+        std::cerr << "Could not connect to server" << std::endl;
+        exit(0);
+    }
+    std::cout << "Connection successful" << std::endl;
+
+    send(client_fd, "Hello", strlen("Hello"), 0);
+
+    return 0;
 
 
 
--- server.cpp
+++ server.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h> 
+#include <unistd.h>
 
 
 int main() {
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_fd < 0) {
+    // AF_NET: specificies IPv4 address family used by server socket
+    // SOCK_STREAM: socket uses TCP connection
+    // 0 indicates bas
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0); 
+    short portNumber = 3000;
+    if (server_fd < 0) {
         std::cerr << "Could not create socket. Please try again.";
         exit(0);
     }
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portNumber);
+    server_address.sin_addr.s_addr = INADDR_ANY;
     
+    int server_start = bind(server_fd, (sockaddr*)&server_address, sizeof(server_address));
 
+    if (server_start < 0) {
+        std::cerr << "Could not start server" << std::endl;
+        exit(0);
+    }
+
+    std::cout << "Server started" << std::endl;
+
+    listen(server_fd, SOMAXCONN);
+    std::cout << "Server listening on port " << portNumber << std::endl;
+
+    sockaddr_in client_address;
+    socklen_t client_address_size = sizeof(client_address);
     
+    int client_connection_socket = accept(server_fd, (struct sockaddr*)&client_address, &client_address_size);
+
+    if (client_connection_socket < 0) {
+        std::cerr << "A client connection failed" << std::endl;
+        exit(0);
+    }
+
+    std::string msg = "Message received!";
+    send(client_connection_socket, msg.c_str(), msg.size(), 0);
+
+    char read_buffer[1024];
+    read(client_connection_socket, read_buffer, sizeof(read_buffer));
+
+    std::cout << client_connection_socket << ": " << read_buffer << std::endl;
+
+
+    close(server_fd);
+    close(client_connection_socket);
+
+    return 0;
 }
